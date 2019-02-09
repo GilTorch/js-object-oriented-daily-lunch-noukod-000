f040fb52a43b3014ea25fe00d421b5e2f6c4b787
Gilbert Torchon <gilbert@noukod.com>
2019-02-09 22:07:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,3 +29,11 @@ class Customer{
     store.customers.push(this);
   }
 }
+
+class Delivery{
+  constructor(name){
+    this.name=name;
+    this.id=++deliveryId;
+    store.deliveries.push(this);
+  }
+}
